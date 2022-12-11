1d1b6ba6c86b9426a2fdea17517cbf6be790e252
Roshan <nkroshankumar@gmail.com>
2022-12-11 23:15:32 +0530

    Rk svg bg added

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -2,10 +2,20 @@ import React from "react"
 import Button from "./Button"
 import CodeText from "./CodeText"
 import Goal from "./Goal"
+import BG from "./../public/Rv.svg"
 
 const HomeHero = () => {
     return (
-        <div className="h-screen w-full  -z-50 flex justify-around items-center  ">
+        <div
+            style={{
+                backgroundImage: `url('Rv.svg')`,
+                backgroundSize: "30%",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "40% 55%",
+                fill: "red",
+            }}
+            className="min-h-screen w-full  -z-50 flex justify-around items-center "
+        >
             <div className="flex flex-col gap-y-4">
                 <CodeText tag="p" type="p" line="single">
                     <p className="">This is</p>
