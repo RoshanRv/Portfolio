c3473b3025bc9a6cde97c20d92476d8a0ceebe62
Roshan <nkroshankumar@gmail.com>
2022-12-27 10:01:55 +0530

    added animation to hero section

--- components/Goal.tsx
+++ components/Goal.tsx
@@ -1,8 +1,14 @@
-import React, { useState, useRef, useEffect } from "react"
+import { motion } from "framer-motion"
 
 const Goal = () => {
     return (
-        <div className="relative w-10/12 transition-all md:w-4/12 -rotate-12 hover:scale-110 hover:rotate-6">
+        <motion.div
+            initial={{ scale: 0 }}
+            whileInView={{ rotate: -12, scale: 0.95 }}
+            whileHover={{ scale: 1.15, rotate: 12 }}
+            transition={{ duration: 0.05, type: "spring", bounce: 1 }}
+            className="relative w-10/12 transition-all md:w-4/12 -rotate-12 hover:rotate-6"
+        >
             <img
                 draggable={false}
                 src="goal.png"
@@ -10,8 +16,8 @@ const Goal = () => {
                 className="border-2 rounded-md shadow-2xl border-sec shadow-black "
             />
             {/*     CURSOR   */}
-            <div className="absolute w-3 h-5 bg-white/80 top-[76%] left-[14%]  cursor "></div>
-        </div>
+            <div className="absolute w-2 md:w-3 h-4 md:h-5 bg-white/80 top-[76%] left-[14%]  cursor "></div>
+        </motion.div>
     )
 }
 
--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -1,8 +1,35 @@
 import React, { ForwardedRef, forwardRef } from "react"
-import { HomeProp } from "../pages"
+import { motion, Variants } from "framer-motion"
 import Button from "./Button"
 import CodeText from "./CodeText"
 import Goal from "./Goal"
+
+//  animation
+export const container: Variants = {
+    hidden: {
+        opacity: 0,
+    },
+
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+}
+
+export const item: Variants = {
+    hidden: {
+        scale: 0,
+    },
+    show: {
+        scale: 1,
+    },
+
+    hover: {
+        scale: 1.1,
+    },
+}
 
 const HomeHero = forwardRef((_prop, ref: ForwardedRef<HTMLDivElement>) => {
     return (
@@ -16,20 +43,35 @@ const HomeHero = forwardRef((_prop, ref: ForwardedRef<HTMLDivElement>) => {
                 backgroundPosition: "40% 55%",
                 fill: "red",
             }}
-            className="flex flex-col items-center justify-center w-full min-h-screen px-3 md:justify-around md:flex-row -z-50 gap-y-10"
+            className="flex flex-col items-center justify-center w-full min-h-screen px-3 md:justify-around md:flex-row -z-50 gap-y-10 "
         >
-            <div className="flex flex-col gap-y-4">
-                <CodeText tag="p" type="p" line="single">
-                    <p className="">This is</p>
-                </CodeText>
-                <CodeText tag="h1" type="head">
-                    <h1 className="pl-6">Roshan Kumar</h1>
-                </CodeText>
-                <CodeText tag="h3" type="sub" line="single">
-                    <p className="">Full-Stack Developer</p>
-                </CodeText>
-                <Button text=" Hire Me " link={"#contact"} />
-            </div>
+            <motion.div
+                variants={container}
+                initial={"hidden"}
+                whileInView={"show"}
+                className="flex flex-col gap-y-6 md:gap-y-8"
+            >
+                <motion.div variants={item} className="">
+                    <CodeText tag="p" type="p" line="single">
+                        <p className="">This is</p>
+                    </CodeText>
+                </motion.div>
+
+                <motion.div className="" variants={item}>
+                    <CodeText tag="h1" type="head">
+                        <h1 className="pl-6">Roshan Kumar</h1>
+                    </CodeText>
+                </motion.div>
+
+                <motion.div className="" variants={item}>
+                    <CodeText tag="h3" type="sub" line="single">
+                        <p className="">Full-Stack Developer</p>
+                    </CodeText>
+                </motion.div>
+                <motion.div className="mt-8 hover:scale-15-" variants={item}>
+                    <Button text=" Hire Me " link={"#contact"} />
+                </motion.div>
+            </motion.div>
             <Goal />
         </div>
     )
